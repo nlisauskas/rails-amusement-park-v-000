b3f88c8c2ca4e5cadc6a1fce3414526739c9e0e9
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-02-14 03:10:03 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_secure_password
+  has_many :rides
+  has_many :attractions, :through => :rides
+
+  def mood
+    if self.happiness > self.nausea
+      return "happy"
+    elsif self.happiness < self.nausea
+      return "sad"
+    end
+  end
 end
