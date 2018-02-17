4eb0abfe4b2873886ae7ddc6abfae3c9ab7261a0
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-02-17 17:18:20 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,39 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+    redirect_to @user
+    else
+      redirect_to signup_path
+    end
+  end
+
+  def show
+    if logged_in?
+    @user = User.find_by(params[:id])
+  else
+      redirect_to root_path
+    end
+  end
+
+  def login
+    @user = User.new
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :password, :password_confirmation)
+  end
+
+  def logged_in?
+    current_user.id == session[:user_id]
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
  root to: 'welcome#home'
- get '/signup', to: 'users#new', as: 'signup'
+ get '/users/new', to: 'users#new', as: 'signup'
+ get '/signin', to: 'users#login'
+ resources :sessions, only: [:create]
+ delete '/logout', to: 'sessions#destroy'
  resources :users, except: :new
 
 end
