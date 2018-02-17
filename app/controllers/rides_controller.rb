6331f3cc366ccc2aa6dabb1d1d5e88cdfdde4120
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-02-17 21:36:01 +0000

    automatically backed up by learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -4,6 +4,8 @@ class RidesController < ApplicationController
     @user = User.find_by(:id => params[:id])
     @attraction = Attraction.find_by(:id => params[:id])
     @ride = Ride.create(:user_id => params[:user_id], :attraction_id => params[:attraction_id])
+    @message = @ride.take_ride
+    redirect_to user_path(@ride.user, :message => @message)
   end
 
 
